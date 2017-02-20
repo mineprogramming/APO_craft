875cd09b837f5942e18fda3c0e73679d6734433d
Igor01 <Ih01@i.ua>
2017-02-20 22:30:10 +0200

    Перенес основные либки в НИДУ, убрал лишнее

--- project/build/main.js
+++ project/build/main.js
@@ -1,9 +1,65 @@
 
-print("Main!!!!!");
+var Util = {}
+
+Util.getWorldSeed = function(){
+    var worldsPath = "/storage/sdcard0/games/com.mojang/minecraftWorlds/";
+    var leveldat = worldsPath+Level.getWorldDir()+"/level.dat";
+    if(!java.io.File(leveldat).exists()){
+        return 0;
+    }
+    var fin = new java.io.FileInputStream(leveldat);
+    var nechs = [];
+    var startSeed = 0;
+    var seed = "";
+    var str = "";
+    var ch;
+    while((ch=fin.read())!=-1){
+        nechs.push(ch);
+        str+=String.fromCharCode(ch);
+    }
+    startSeed = str.split("RandomSeed")[0].length+10;
+    for(var i=3;i>=0;i--){
+        if(nechs[startSeed+i]<16){
+            seed+="0"+nechs[startSeed+i].toString(16)+"";
+        }else{
+            seed+=nechs[startSeed+i].toString(16)+"";
+        }
+    }
+    var endSeed = parseInt(seed, 16);
+    if(endSeed>(Math.pow(16,8)/2-1)){
+        return endSeed-Math.pow(16,8);
+    }
+    return endSeed;
+};
+
+Util.convertStreamToString = function(is) { 
+    var bis = new java.io.BufferedInputStream(is); 
+    var buf = new java.io.ByteArrayOutputStream(); 
+    var res = bis.read(); 
+    while(res != -1) { 
+        buf.write(res); 
+        res = bis.read(); 
+    } 
+    return buf.toString(); 
+}
+var Randomizer = {
+    Random: (new java.util.Random(Util.getWorldSeed()))
+};
+
+Randomizer.GaussRandom = function(max, depth){
+    if (typeof depth === 'undefined') {
+        depth = 1;
+    }
+    var result = 0;
+    for(var i = 0; i < depth; i++){
+        result += this.Random.nextInt(max * 2) - max;
+    }
+    return Math.round(Math.abs(result / depth));
+};
 var Timers = {};
 
 Timers.modTick = function(){
-
+    
 }
 
 Timers.addOnce = function(){
@@ -15,20 +71,9 @@ Timers.addRepetiteve = function(){
 }
 ItemsEngine = {};
 
-ItemsEngine.convertStreamToString = function(is) { 
-    var bis = new java.io.BufferedInputStream(is); 
-    var buf = new java.io.ByteArrayOutputStream(); 
-    var res = bis.read(); 
-    while(res != -1) { 
-        buf.write(res); 
-        res = bis.read(); 
-    } 
-    return buf.toString(); 
-}
-
 ItemsEngine.SetItemFromJson = function(name){
     var str    = ModPE.openInputStreamFromTexturePack("items//" + name);
-    var string = convertStreamToString(str);
+    var string = Util.convertStreamToString(str);
     var json   = JSON.parse(string);
     if (json.type == "item"){
         ModPE.setItem(json.id, json.texture.name, json.texture.meta, json.name, json.maxStack);
@@ -46,4 +91,4 @@ ItemsEngine.SetItemFromJson = function(name){
     else if (json.type == "throwable")
         Item.defineThrowable(json.id, json.texture.name, json.texture.meta, json.name, json.maxStack);
 }
-var Library = {};
+
