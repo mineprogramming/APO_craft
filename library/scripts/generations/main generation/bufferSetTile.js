e213229de3e3ad7f83b8507a2b4f2594576cb562
maksim-vort <maksimruk2002n2@gmail.com>
2017-02-24 22:41:38 +0300

    Новые идеи и генерация

---
+++ library/scripts/generations/main generation/bufferSetTile.js
@@ -0,0 +1,30 @@
+var buffer=[];
+var build_speed=20;
+function bufferSetTile(x,y,z,id,data,important)
+{
+	if(important)
+	{
+		setTile(x,y,z,id,data);
+	}else{
+		buffer.push({x:x,y:y,z:z,i:id,d:data});
+	}
+}
+function bufferTick()
+{
+	for(var i=0;i<Math.min(buffer.length,build_speed);i++)
+	{
+		var b=buffer[0];
+		defaultSetTile(b.x,b.y,b.z,b.i,b.d);
+		buffer.shift();
+	}
+}
+function useItem(x,y,z,i)
+{
+	if(i==280)
+	{
+		for(var i=0;i<100;i++)
+		{
+			bufferSetTile(x+i,y+i,z+i,57,0,false);
+		}
+	}
+}
