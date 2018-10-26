abc96697ddcdf65c06adaed51b3546ad90f469fa
IchZerowan <Ih01@i.ua>
2018-10-26 16:19:39 +0300

    Added Coke Oven

--- dev/core/IC2Integration.js
+++ dev/core/IC2Integration.js
@@ -1,12 +1,5 @@
 var ICore = false;
 ModAPI.addAPICallback("ICore", function(api){ 
     ICore = api;
-    
-    //Scrap from Waste
-    RecipiesManager.addShaped({id: ItemID.scrap, count: 1, data: 0}, [
-        "aaa",
-        "aaa",
-        "aaa"
-    ], ['a', ItemID.waste, 0]);
 
 });
